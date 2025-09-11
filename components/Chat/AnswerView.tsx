9e265121762be1688af0201b30cda7e22341bf08
clabi-darye <darye@clabi.co.kr>
2025-09-11 13:50:52 +0900

    Feat: LaTeX 수식 스타일 변경

--- components/Chat/AnswerView.tsx
+++ components/Chat/AnswerView.tsx
@@ -5,6 +5,7 @@ import clsx from "clsx";
 import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
 type AnswerViewProps = {
   streamText: string;
